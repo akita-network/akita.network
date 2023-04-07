6d4d739689766f5c76a640b540a47b81a75128c7
kristiansalov <k.salov10@gmail.com>
2023-04-07 22:50:51 +0200

    Updated the tokenomics groups section

--- src/components/sections/tokenomics/images.tsx
+++ src/components/sections/tokenomics/images.tsx
@@ -12,11 +12,15 @@ const Images = ({
     noPadding = false
 }: IImages) => (
     <div className="mt-56 mb-56 xl:mb-0 xl:mt-0 md:w-1/2 md:mx-auto relative flex items-center justify-center">
-        <img className={`absolute ${!noPadding ? "p-8" : ""}`} src={`/assets/tokenomics/${group}/groupinitial.svg`} />
-        <img
-            className={`absolute transition-opacity duration-500 opacity-0 ${selected > -1 ? "opacity-100" : ""} ${!noPadding ? "p-8" : ""}`}
-            src={`/assets/tokenomics/${group}/group${selected > -1 ? selected : "initial"}.svg`}
-        />
+        <img className={`absolute ${!noPadding ? "p-8" : ""}`} src={`/assets/tokenomics/${group}/initial.svg`} />
+
+        {selected > -1 && (
+            <img
+                className={`absolute ${!noPadding ? "p-8" : ""}`}
+                src={`/assets/tokenomics/${group}/${selected}.svg`}
+            />
+        )}
+
     </div>
 )
 
